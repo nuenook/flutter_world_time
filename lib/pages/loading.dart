2232ec9d011973cc5ffa3435af5a0686885f7fc1
nuenook <nook.ittipon@gmail.com>
2019-11-22 09:23:45 +0700

    add worldtime API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,25 +8,32 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  String time = 'loading';
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['userId']);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Thailand', flag: 'thai.png', url: 'Asia/Bangkok');
+    await instance.getTime();
+    print(instance.time);
+
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    
-    getData();
+    setupWorldTime();
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
